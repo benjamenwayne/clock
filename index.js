e439070f1f5af3e02db68f54a367e14151261dac
Ben Kautz <benjamenwayne@icloud.com>
2023-09-19 18:25:39 -0500

    Added time swap button. Fixed clerical errors.

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-
-// let updateClock = () =>{
-//     let date = new Date()
-//     let hours = date.getHours();
-//     let minutes = date.getMinutes();
-//     let seconds = date.getSeconds();
-//     let currentTime = hours + ":" + minutes + ":" + seconds
-//     document.getElementById("clock").innerHTML = currentTime
-// }
-// setInterval(updateClock, 1000)
-
-
-
+let militaryTime = true;
+const element = document.getElementById("military-standard");
+element.addEventListener("click", changeTime);
+function changeTime() {
+    if (!militaryTime) {
+        militaryTime = true;
+        document.getElementById("military-standard").innerHTML = "Display Standard Time";
+    }  
+    else {
+        militaryTime = false;
+        document.getElementById("military-standard").innerHTML = "Display Military Time";
+    }
+}
 function updateClock() {
     const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
 
-    // Update the HTML elements with the extracted time values
+    if (!militaryTime) {
+        if (hours > 12) {
+            hours -= 12;
+        }
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds
+    }
     document.getElementById("time").innerHTML = hours + ":" + minutes + ":" + seconds
-
 }
 setInterval(updateClock, 1000);
-
