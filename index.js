7f4bdc642a4a674f903cde4b10077f6e76af9729
Ben Kautz <benjamenwayne@icloud.com>
2023-09-19 19:26:02 -0500

    Added year, month, and day.

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ function changeTime() {
 }
 function updateClock() {
     const now = new Date();
+    const monthNames = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+      ];
+    let year = now.getFullYear();
+    let month = monthNames[now.getMonth()];
+    let day = now.getDate();
     let hours = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
@@ -28,6 +35,6 @@ function updateClock() {
     if (seconds < 10) {
         seconds = "0" + seconds
     }
-    document.getElementById("time").innerHTML = hours + ":" + minutes + ":" + seconds
+    document.getElementById("time").innerHTML = year + ", " + month + " " + day + ", " + hours + ":" + minutes + ":" + seconds
 }
 setInterval(updateClock, 1000);
